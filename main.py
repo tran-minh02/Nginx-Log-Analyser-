301aa8a4fa4f62a2a3a6e26c5356bc5de6b19ead
Tran Cong Minh <trancongminh12a1@gmail.com>
2025-01-23 22:31:15 +0700

    Thiếu vận hành

--- main.py
+++ main.py
@@ -1,45 +1,67 @@
 from collections import defaultdict
 import time 
 import itertools
+from tabulate import tabulate
+import os , sys
+import subprocess 
+
 ip = defaultdict(int)
 path = defaultdict(int)
 status_code = defaultdict(int)
 user_agent = defaultdict(int)
+log_file_path = r"/var/log/nginx/access.log"
 
-def count(input_log):
-    ip[lst[0]] += 1
-    path[lst[1]] += 1
-    status_code[lst[2]] += 1
-    user_agent[lst[3]] += 1
+def check_file_part(log_part):
+    if os.path.exists(log_part):
+        return log_part
+    else: 
+        log_part = r"sample-nginx-log.txt"
+        print("Đây là kết quả mẫu đến từ input của Roadmap.sh")
+        return log_part
+
+def check_nginx_service():       
+    output = os.popen("systemctl is-active nginx").read().strip()
+    if output == "active":
+        pass
+    else:
+        print("Hệ thống không chạy máy chủ Nginx")
+        # sys.exit()
     
-def sort_dict(dict):
-    return dict(sorted(dict.items(), key=lambda item: item[1], reverse=True))
+def count(input_log):
+    ip[input_log[0]] += 1
+    path[input_log[1]] += 1
+    status_code[input_log[2]] += 1
+    user_agent[input_log[3]] += 1
 
 def output(dic):
-    for x in dict(itertools.islice(dic.items(), 5)):
-        print(f"{x} - {dic[x]} requests")
-        
-with open("sample-nginx-log.txt", "r") as f:
-    while True:
-        try:
-            input_line = f.readline().strip().split(" ")
-            lst = [input_line[0],input_line[6],input_line[8]," ".join(input_line[11:])]
-            count(lst)
-        except IndexError as e : 
-            # time.sleep(60)
-            # continue
-            break
-print('-------------------------------------------------')
-print("Top 5 IP addresses with the most requests:")
-output(ip)
-print('-------------------------------------------------')
-print("Top 5 most requested paths:")
-output(path)
-print('-------------------------------------------------')
-print("Top 5 response status codes:")
-output(status_code)
-print('-------------------------------------------------')
-print("Top 5 IP addresses with the most requests:")
-output(user_agent)
-print('-------------------------------------------------')
+    header = [1,2]
+    result = tabulate(list(itertools.islice(dic.items(), 5)), tablefmt="fancy_grid")
+    print(result)
+
+def monitor_log():
+    check_nginx_service()
+    log_file = check_file_part(log_file_path)
+    with open(log_file, "r") as f:
+        try: 
+            while True:
+                input_line = f.readline().strip().split(" ")
+                lst = [input_line[0],input_line[6],input_line[8]," ".join(input_line[11:])]
+                count(lst)
+        except Exception as e :
+            pass
+    print('-------------------------------------------------')
+    print("Top 5 IP addresses with the most requests:")
+    output(ip)
+    print('-------------------------------------------------')
+    print("Top 5 most requested paths:")
+    output(path)
+    print('-------------------------------------------------')
+    print("Top 5 response status codes:")
+    output(status_code)
+    print('-------------------------------------------------')
+    print("Top 5 IP addresses with the most requests:")
+    output(user_agent)
+    print('-------------------------------------------------')
     
+if __name__ == "__main__":
+    monitor_log()
--- solusion.sh
+++ solusion.sh
@@ -1,10 +1,17 @@
-echo "Top 5 IP addresses with the most requests:" 
-awk '{print $1}' sample-nginx-log.txt | sort | uniq -c | sort -nr | head -n 5
-echo "Top 5 most requested paths"
-awk '{print $7}' sample-nginx-log.txt | sort | uniq -c | sort -nr | head -n 5
-echo "Top 5 most requested paths"
-awk '{print $7}' sample-nginx-log.txt | sort | uniq -c | sort -nr | head -n 5
-
-
-sort sample-nginx-log.txt | uniq -c | awk '{printf "%-10s %s\n", $2, $1}' | sort -nr | head -n 5
-
+# echo "Top 5 IP addresses with the most requests:" 
+# awk '{print $1}' sample-nginx-log.txt | sort | uniq -c | sort -nr | head -n 5 | awk '{printf "%s - %d requests\n", $2, $1}'
+# echo "" 
+# echo "Top 5 most requested paths"
+# awk '{print $7}' sample-nginx-log.txt | sort | uniq -c | sort -nr | head -n 5 | awk '{printf "%s - %d requests\n", $2, $1}'
+# echo ""
+# echo "Top 5 most requested status "
+# awk ' {if (NF == 10) print $7; else print $9;}' sample-nginx-log.txt | sort | uniq -c | sort -nr | head -n 10 | awk '{printf "%s - %d requests\n", $2, $1}'
+# echo ""
+# echo "Top 5 most requested user agents\n"
+# awk '{for (i=12; i<=NF; i++) printf "%s ", $i; print ""}' sample-nginx-log.txt | sort | uniq -c | sort -nr | head -n 5 | awk '{printf "%s - %d requests\n", $2, $1}'
+# awk '{print $1}' sample-nginx-log.txt : Sẽ loc và lấy toàn bộ cột 1 
+# sort : sắp xếp mặc định theo thứ tự a-->z 
+# uniq -c sẽ lọc các dự liệu trùng lặp và trả về số lần xuất hiện của nó
+# sort -nr : tùy chọn -n sắp xếp theo thứ tự Số | tùy chọn -r sắp xếp ngược lại từ lớn đến bé
+# head -n 5 : -n cho phép đọc 5 dòng đầu tiên của đầu vào . 
+# awk '{print NF}' sample-nginx-log.txt
